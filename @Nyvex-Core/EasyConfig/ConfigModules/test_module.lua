60da58e07481b74d404d85b35c644a6f7eada80e
Preakrust <118760041+Preakrust@users.noreply.github.com>
2023-05-07 18:55:56 +0200

    Update test_module.lua

--- @Nyvex-Core/EasyConfig/ConfigModules/test_module.lua
+++ @Nyvex-Core/EasyConfig/ConfigModules/test_module.lua
@@ -3,7 +3,7 @@ return {
 	["src/DynamicSource"] = "https://raw.githubusercontent.com/Preakrust/Nyvex-Core/main/%40Nyvex-Core/EasyConfig/Client.lua",
 	
 	["Module/Id"] = "SomeGoofyProduct",
-	["Module/AuthoriId"] = "3406870812",
+	["Module/AuthorId"] = "3406870812",
 	
 	["System/Icon"] = "rbxassetid://6750074875",
 	["System/DisplayName"] = "Goofy Ahh Product",
