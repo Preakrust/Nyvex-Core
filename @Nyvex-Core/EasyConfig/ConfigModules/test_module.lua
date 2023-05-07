757e481621eaee179c696422186a068c05d49380
Preakrust <118760041+Preakrust@users.noreply.github.com>
2023-05-07 16:44:12 +0200

    Create test_module.lua

---
+++ @Nyvex-Core/EasyConfig/ConfigModules/test_module.lua
@@ -0,0 +1,6 @@
+return {
+	["src/UseDynamicSource"] = false,
+	["src/DynamicSource"] = "https://raw.githubusercontent.com/Preakrust/Nyvex-Core/main/%40Nyvex-Core/EasyConfig/Client.lua",
+	AuthorUserId = "3406870812",
+	Description = "Test lol"
+}
