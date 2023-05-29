ad6df8f1bc94c9f135f6457a944fd8ce9a8ee95c
Preakrust <118760041+Preakrust@users.noreply.github.com>
2023-05-29 13:37:01 +0200

    Update index.js

--- Website/api/v3/auth/connect/index.js
+++ Website/api/v3/auth/connect/index.js
@@ -1,1 +1,39 @@
-console.log("HdiHi")
+
+// Dependencies //
+
+var SimpleNotifyScript = document.createElement('script');
+var SimpleNotifyCSS = document.createElement("link");
+
+SimpleNotifyCSS.rel = "stylesheet";
+
+SimpleNotifyCSS.setAttribute('href', "https://cdn.jsdelivr.net/npm/simple-notify@0.5.5/dist/simple-notify.min.css");
+SimpleNotifyScript.setAttribute('src',"https://cdn.jsdelivr.net/npm/simple-notify@0.5.5/dist/simple-notify.min.js");
+
+document.head.appendChild(SimpleNotifyScript);
+document.head.appendChild(SimpleNotifyCSS);
+
+// Main code //
+
+let myNotify
+
+function startWidget() {
+  myNotify = new Notify({
+    status: 'success',
+    title: 'This website supports Nyvex-Core Connect!',
+    text: 'Click the button',
+    effect: 'fade',
+    speed: 300,
+    customClass: null,
+    customIcon: null,
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: false,
+    autotimeout: 3000,
+    gap: 20,
+    distance: 20,
+    type: 1,
+    position: 'right top'
+  })
+}
+
+startWidget()
